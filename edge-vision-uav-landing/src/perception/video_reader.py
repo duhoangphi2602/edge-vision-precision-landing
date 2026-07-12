1a0b6ad8a69a2eed0f688ef1e7d6b91dabaef99c
Chisaii <duhoangphi2617@gmail.com>
2026-07-12 19:21:52 +0700

    fix webcam

--- edge-vision-uav-landing/src/perception/video_reader.py
+++ edge-vision-uav-landing/src/perception/video_reader.py
@@ -2,7 +2,10 @@ import cv2
 
 class VideoReader:
     def __init__(self, source=0, width=640, height=480):
-        self.cap = cv2.VideoCapture(source)
+        # Thêm cờ CAP_V4L2 để fix lỗi timeout trên Linux
+        self.cap = cv2.VideoCapture(source, cv2.CAP_V4L2)
+        # Ép camera xuất định dạng YUYV vì luồng MJPG đang bị nhiễu và vỡ ảnh (Corrupt JPEG data)
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'YUYV'))
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
